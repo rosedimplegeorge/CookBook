6d61524866ec9765b24eaeea9542ce84b24d393f
rosedimple <rosedimplegeorge@gmail.com>
2018-04-29 16:29:13 -0400

    Server created

---
+++ server.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const app = express();
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGODB_URI);
+
+const connection = mongoose.connection;
+connection.on('connected', () => {
+  console.log('Mongoose Connected Successfully');    
+}); 
+
+connection.on('error', (err) => {
+  console.log('Mongoose default connection error: ' + err);
+}); 
+
+app.use(bodyParser.json());
+app.get('/', (req,res) => {
+  res.send('Hello world!')
+})
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+    console.log('================================')
+    console.log("Express Server is on port " + PORT);
+    console.log('================================')
+})
